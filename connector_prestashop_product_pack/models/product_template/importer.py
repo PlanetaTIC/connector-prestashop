d7316fdb170c1c9ade3e3ae0bab8c59d3ba57828
Marc Poch <mpoch@planetatic.com>
2021-06-08 10:48:09 +0200

    [FIX] connector_prestashop_product_pack: Now working when importing new products and when updating already mapped products.

--- connector_prestashop_product_pack/models/product_template/importer.py
+++ connector_prestashop_product_pack/models/product_template/importer.py
@@ -5,33 +5,41 @@ from odoo import _, models, api
 from odoo.addons.component.core import Component
 from odoo.addons.connector.components.mapper import (
     mapping, external_to_m2o, only_create)
+from odoo.addons.queue_job.exception import FailedJobError
 
 
 class ProductTemplateImporter(Component):
     _inherit = 'prestashop.product.template.importer'
 
+    def _import_dependencies(self):
+        super(ProductTemplateImporter, self)._import_dependencies()
+        self._import_bundle_dependencies()
+
+    def _import_bundle_dependencies(self):
+        # Import products included in the bundle (product_pack):
+        prestashop_record = self.prestashop_record
+        associations = prestashop_record.get('associations', {})
+
+        ps_key = self.backend_record.get_version_ps_key('product_bundle')
+        bundle = associations.get('product_bundle', {}).get(ps_key, [])
+
+        if not isinstance(bundle, list):
+            bundle = [bundle]
+        for ps_packed_product in bundle:
+            if ps_packed_product.get('id_product_attribute', '0') == '0':
+                self._import_dependency(
+                    ps_packed_product['id'],
+                    'prestashop.product.template',
+                    always=True)
+            else:
+                self._import_dependency(
+                    ps_packed_product['id_product_attribute'],
+                    'prestashop.product.combination',
+                    always=True)
+
     def _after_import(self, binding):
         super(ProductTemplateImporter, self)._after_import(binding)
         self.import_bundle(binding)
-
-    def _import_bundle(self, bundle, **kwargs):
-        # We need to pass the template presta record because we need it
-        # for combination mapper
-        self.work.parent_presta_record = self.prestashop_record
-        if 'parent_presta_record' not in self.work._propagate_kwargs:
-            self.work._propagate_kwargs.append('parent_presta_record')
-        if bundle.get('id_product_attribute', '0') == '0':
-            self._import_dependency(
-                bundle['id'],
-                'prestashop.product.template',
-                always=True,
-                **kwargs)
-        else:
-            self._import_dependency(
-                bundle['id_product_attribute'],
-                'prestashop.product.combination',
-                always=True,
-                **kwargs)
 
     def import_bundle(self, binding):
         prestashop_record = self._get_prestashop_data()
@@ -51,7 +59,6 @@ class ProductTemplateImporter(Component):
         for ps_packed_product in bundle:
             pack_ok = True
             pack_type = 'non_detailed'
-            self._import_bundle(ps_packed_product)
 
             packed_product_tmpl = template_binder.to_internal(
                 ps_packed_product['id'],
@@ -73,3 +80,46 @@ class ProductTemplateImporter(Component):
             'pack_type': pack_type,
             'pack_line_ids': pack_lines_vals
         })
+
+
+class TemplateMapper(Component):
+    _inherit = 'prestashop.product.template.mapper'
+
+    @mapping
+    def pack_line_ids(self, record):
+        ps_packed_products = record['associations'].get('product_bundle', {}).\
+            get(self.backend_record.get_version_ps_key('product'), [])
+        if not isinstance(ps_packed_products, list):
+            ps_packed_products = [ps_packed_products]
+        if not ps_packed_products:
+            return {
+                'pack_ok': False,
+                'pack_line_ids': False,
+            }
+
+        product_tmpl_binder = self.binder_for('prestashop.product.template')
+        combination_binder = self.binder_for('prestashop.product.combination')
+        pack_lines_vals = [(5, False, False)]
+        for ps_packed_product in ps_packed_products:
+            packed_product_tmpl = product_tmpl_binder.to_internal(
+                ps_packed_product['id'],
+                unwrap=True,
+            )
+            if ps_packed_product.get('id_product_attribute', '0') == '0':
+                packed_product = packed_product_tmpl.product_variant_ids[0]
+            else:
+                # get product combination:
+                packed_product = combination_binder.to_internal(
+                    ps_packed_product['id_product_attribute'])
+            if not packed_product:
+                raise FailedJobError(_("Missing packed product"))
+            pack_line_vals = {
+                'product_id': packed_product.id,
+                'quantity': ps_packed_product['quantity'],
+            }
+            pack_lines_vals.append((0, False, pack_line_vals))
+        res = {
+            'pack_ok': True,
+            'pack_type': 'non_detailed',
+            'pack_line_ids': pack_lines_vals}
+        return res
